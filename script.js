1eecea83a4ed243e9d2f8c02ffc16778f57941ac
seema-io <seemanazreennagoor@gmail.com>
2026-07-28 10:25:00 +0530

    Final portfolio

--- script.js
+++ script.js
@@ -98,3 +98,36 @@ window.addEventListener("scroll", function () {
     }
 
 });
+// Active Navigation Highlight
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+
+    let currentSection = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.offsetHeight;
+
+        if (
+            window.scrollY >= sectionTop &&
+            window.scrollY < sectionTop + sectionHeight
+        ) {
+            currentSection = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + currentSection) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
